c9df5ca9edb986183d5961bdd2582c65a5058cbe
CharlyB03 <charly.bazan.2003@outlook.es>
2026-02-10 15:38:15 -0500

    [FEAT] DB41 Desarrollo

--- modules/ec.com.sidesoft.happypay.web.services/src/ec/com/sidesoft/happypay/web/services/ad_process/DaysToCleanLog.java
+++ modules/ec.com.sidesoft.happypay.web.services/src/ec/com/sidesoft/happypay/web/services/ad_process/DaysToCleanLog.java
@@ -20,70 +20,70 @@ import ec.com.sidesoft.actuaria.special.customization.Scactu_Log;
 import ec.com.sidesoft.happypay.web.services.shppws_config;
 import org.openbravo.base.exception.OBException;
 
-public class DaysToCleanLog extends DalBaseProcess{
+public class DaysToCleanLog extends DalBaseProcess {
 
 	private static ProcessLogger logger;
-	
+
 	@Override
 	protected void doExecute(ProcessBundle bundle) throws Exception {
 		// TODO Auto-generated method stub
 		logger = bundle.getLogger();
 		ConnectionProvider conn = bundle.getConnection();
 		try {
-			OBCriteria<shppws_config> queryUrl= OBDal.getInstance().createCriteria(shppws_config.class);
-		    List<shppws_config> listUrl= queryUrl.list();
-		    
-		    if(listUrl.size() > 0) {
-		    	Long daysValidatorAux = listUrl.get(0).getDaysCleanlog();
-		    	int daysValidator = daysValidatorAux != null ? daysValidatorAux.intValue() : 0;
-		    	if(daysValidator != 0 && daysValidator > 0) {
-		    		// fecha límite
-		    	    LocalDate limitDate = LocalDate.now().minusDays(daysValidator);
-		    	    
-		    	    Date limitDateAsDate = Date.from(limitDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-		    	    
-		    	    delete(conn, limitDateAsDate);
-		    		
-		    	}else {
-		    		logger.logln("No hay coincidencia de registros para ser eliminados.");
-		    	}
-		    }else {
-		    	logger.logln("No hay coincidencia de registros para ser eliminados.");
-		    }
-		}catch(Exception e) {
+			OBCriteria<shppws_config> queryUrl = OBDal.getInstance().createCriteria(shppws_config.class);
+			List<shppws_config> listUrl = queryUrl.list();
+
+			if (listUrl.size() > 0) {
+				Long daysValidatorAux = listUrl.get(0).getDaysCleanlog();
+				int daysValidator = daysValidatorAux != null ? daysValidatorAux.intValue() : 0;
+				if (daysValidator != 0 && daysValidator > 0) {
+					// fecha límite
+					LocalDate limitDate = LocalDate.now().minusDays(daysValidator);
+
+					Date limitDateAsDate = Date.from(limitDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+					delete(conn, limitDateAsDate);
+
+				} else {
+					logger.logln("No hay coincidencia de registros para ser eliminados.");
+				}
+			} else {
+				logger.logln("No hay coincidencia de registros para ser eliminados.");
+			}
+		} catch (Exception e) {
 			logger.logln("No se ha podido completar el proceso /" + e.getMessage());
 			throw new OBException("Error al limpiar los registros de la tabla log de servicios web.");
 		}
 	}
-	
+
 	public void delete(ConnectionProvider conn, Date date) {
-	    logger.logln("Eliminando registros con fecha menor a "+date+" :");
-	    int updateCount = 0;
+		logger.logln("Eliminando registros con fecha menor a " + date + " :");
+		int updateCount = 0;
 
-	    String sql = "DELETE FROM scactu_log WHERE created < ?";
+		String sql = "DELETE FROM scactu_log WHERE created < ?";
 
-	    PreparedStatement st = null;
-	    try {
-	        st = conn.getPreparedStatement(sql);
-	        st.setDate(1, new java.sql.Date(date.getTime())); // Convertimos java.util.Date a java.sql.Date
-	        QueryTimeOutUtil.getInstance().setQueryTimeOut(st, SessionInfo.getQueryProfile());
-	        updateCount = st.executeUpdate();
+		PreparedStatement st = null;
+		try {
+			st = conn.getPreparedStatement(sql);
+			st.setDate(1, new java.sql.Date(date.getTime())); // Convertimos java.util.Date a java.sql.Date
+			QueryTimeOutUtil.getInstance().setQueryTimeOut(st, SessionInfo.getQueryProfile());
+			updateCount = st.executeUpdate();
 
-	        if (updateCount > 0) {
-	        	logger.logln("Depuración exitosa de logs. Registros eliminados: " + updateCount);
-	        } else {
-	            logger.logln("No se encontraron registros para la depuración de logs");
-	        }
-	    } catch (Exception e) {
-	        logger.logln("Error ejecutando delete: " + sql + " - " + e.getMessage());
-	    } finally {
-	        try {
-	            if (st != null) st.close();
-	        } catch (Exception e) {
-	            logger.logln("Error cerrando PreparedStatement: " + e.getMessage());
-	        }
-	    }
+			if (updateCount > 0) {
+				logger.logln("Depuración exitosa de logs. Registros eliminados: " + updateCount);
+			} else {
+				logger.logln("No se encontraron registros para la depuración de logs");
+			}
+		} catch (Exception e) {
+			logger.logln("Error ejecutando delete: " + sql + " - " + e.getMessage());
+		} finally {
+			try {
+				if (st != null)
+					st.close();
+			} catch (Exception e) {
+				logger.logln("Error cerrando PreparedStatement: " + e.getMessage());
+			}
+		}
 	}
-
 
 }
